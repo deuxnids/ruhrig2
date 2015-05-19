aa32f706778c7566ee8e900137446990a2419b87
deuxnids <denism@student.ethz.ch>
2015-05-19 20:47:24 +0200

    init commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://ruhrig:ruhrighongg@ds037622.mongolab.com:37622/ruhrig'
+  },
+
+  seedDB: true
+};
